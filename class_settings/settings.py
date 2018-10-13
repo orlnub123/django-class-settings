1d3da83f884e3970f2dfafc3bdc0ecebd8bfa259
orlnub123 <orlnub123@gmail.com>
2018-10-13 14:12:55 +0300

    Allow changing default settings

--- class_settings/settings.py
+++ class_settings/settings.py
@@ -4,9 +4,24 @@ import os
 from django.conf import global_settings, settings
 
 
-class Settings:
+class Options:
+    def __init__(self, meta):
+        self.default_settings = getattr(meta, "default_settings", global_settings)
+
+
+class SettingsMeta(type):
+    def __init__(cls, name, bases, namespace):
+        meta = getattr(cls, "Meta", None)
+        cls._meta = Options(meta)
+
+
+class Settings(metaclass=type("Meta", (type,), {})):  # Hack for __class__ assignment
     def __getattr__(self, name):
-        return getattr(global_settings, name)
+        default_settings = self._meta.default_settings
+        return getattr(default_settings, name)
+
+
+Settings.__class__ = SettingsMeta
 
 
 def setup():
