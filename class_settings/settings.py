9691803a366321d057adf48a8f1b776e1ac6d690
orlnub123 <orlnub123@gmail.com>
2018-10-14 11:31:44 +0300

    Support manage.py --settings
    This allows you to change your settings class (and module) via `manage.py --settings path.to.settings:Settings`.

--- class_settings/settings.py
+++ class_settings/settings.py
@@ -3,6 +3,8 @@ import os
 
 from django.conf import global_settings, settings
 
+from .utils import _patch_settings_setup
+
 
 class Options:
     def __init__(self, meta):
@@ -24,8 +26,12 @@ class Settings(metaclass=type("Meta", (type,), {})):  # Hack for __class__ assig
 Settings.__class__ = SettingsMeta
 
 
+@_patch_settings_setup  # Needed for manage.py --settings support
 def setup():
     module_path = os.environ["DJANGO_SETTINGS_MODULE"]
-    cls_path = os.environ["DJANGO_SETTINGS_CLASS"]
+    if ":" in module_path:
+        module_path, cls_path = module_path.split(":", maxsplit=1)
+    else:
+        cls_path = os.environ["DJANGO_SETTINGS_CLASS"]
     cls = getattr(importlib.import_module(module_path), cls_path)
     settings.configure(cls())
