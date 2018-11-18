fc9c2ee0d9a80f8f64239cc3533e321771682af3
orlnub123 <orlnub123@gmail.com>
2018-11-18 07:42:01 +0300

    [0.1] Fix normalized prefixes not being uppercased
    (cherry picked from commit a4a4b3cf402dd4d28550f6793917021ed60ce113)

--- class_settings/env.py
+++ class_settings/env.py
@@ -8,6 +8,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 from . import parsers
 from .options import Options
+from .utils import normalize_prefix
 
 
 class Missing:
@@ -44,15 +45,13 @@ class Env:
 
         if name is None:
             return DeferredEnv(self, prefix=prefix, default=default)
-        prefix = (
+        prefix = normalize_prefix(
             prefix
             if prefix is not None
             else self._prefix
             if self._prefix is not None
             else options.env_prefix
         )
-        if prefix.islower() and not prefix.endswith("_"):
-            prefix += "_"
         name = prefix + name if prefix is not None else name
         if default is not missing:
             return os.environ.get(name, default)
@@ -74,8 +73,7 @@ class Env:
 
     @contextlib.contextmanager
     def prefixed(self, prefix):
-        if prefix.islower() and not prefix.endswith("_"):
-            prefix += "_"
+        prefix = normalize_prefix(prefix)
         old_prefix = self._prefix
         if old_prefix is None:
             self._prefix = prefix
