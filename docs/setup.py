e778d222e00ba4ebf197a6cd854f4904a95becba
orlnub123 <orlnub123@gmail.com>
2018-12-12 18:17:33 +0300

    Add lxml to install_requires

--- docs/setup.py
+++ docs/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="docs",
     packages=find_packages(),
-    install_requires=["mkdocs~=1.0", "markdown<3", "py-gfm"],
+    install_requires=["mkdocs~=1.0", "markdown<3", "py-gfm", "lxml"],
     entry_points={
         "mkdocs.plugins": ["index_generator = plugins.generators:IndexGeneratorPlugin"]
     },
