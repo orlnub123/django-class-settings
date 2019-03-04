b3ddc2df9c1062241e884e4b3cb99308e674fdda
orlnub123 <orlnub123@gmail.com>
2019-03-04 17:34:29 +0300

    Add missing brackets to release and bump prefixes

--- tasks.py
+++ tasks.py
@@ -164,7 +164,7 @@ def release(c, publish=False, bump=""):
     if current_branch == "master":
         c.run(f"git commit -am 'Release {releaser.release_version}'")
     else:
-        release_prefix = current_branch[len("release/") :]
+        release_prefix = f"[{current_branch[len('release/') :]}]"
         c.run(f"git commit -am '{release_prefix} Release {releaser.release_version}'")
     c.run(
         f"git tag -am 'django-class-settings {releaser.release_version}' "
@@ -196,7 +196,7 @@ def bump(c, version):
     if current_branch == "master":
         c.run(f"git commit -am 'Bump version to {bumper.bump_version}'")
     else:
-        bump_prefix = current_branch[len("release/") :]
+        bump_prefix = f"[{current_branch[len('release/') :]}]"
         c.run(f"git commit -am '{bump_prefix} Bump version to {bumper.bump_version}'")
 
     c.run("git checkout master")
