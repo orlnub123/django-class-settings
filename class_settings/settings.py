c1cd7d2af63c4a5ae980f1cf3ae4cee6642ca5b2
orlnub123 <orlnub123@gmail.com>
2019-03-08 18:35:28 +0300

    [0.1] Fix indented Settings subclasses erroring
    (cherry picked from commit 4512ce16cca9564ba28ed0835b0d8fc48f59ba02)

--- class_settings/settings.py
+++ class_settings/settings.py
@@ -3,6 +3,7 @@ import __future__
 import ast
 import inspect
 import sys
+import textwrap
 import tokenize
 
 from .env import DeferredEnv
@@ -23,6 +24,7 @@ class SettingsMeta(type):
         with tokenize.open(filename) as file:
             lines = file.readlines()[frame.f_lineno - 1 :]
         source = "".join(inspect.getblock(lines))
+        source = textwrap.dedent(source.expandtabs())
 
         cls_node = ast.parse(source).body[0]
         for node in reversed(list(ast.iter_child_nodes(cls_node))):
